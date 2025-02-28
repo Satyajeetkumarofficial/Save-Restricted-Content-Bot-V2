2268c8858a9b6c6d24d00cbba2cde0edd587b862
Satyajeetkumarofficial <152394869+Satyajeetkumarofficial@users.noreply.github.com>
2025-03-01 00:35:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,13 +13,13 @@ YTUB_COOKIES = """
 # write here yt cookies
 """
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
-MONGO_DB = getenv("MONGO_DB", "")
-LOG_GROUP = getenv("LOG_GROUP", "")
-CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
+API_ID = int(getenv("API_ID", "25617967"))
+API_HASH = getenv("API_HASH", "10555bea1cdfc7d2303fc13b7fd187cc")
+BOT_TOKEN = getenv("BOT_TOKEN", "7998052892:AAHzGUJejSOG4JVYknbYkrJl-6B-xOL3I0g")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "8020340814").split()))
+MONGO_DB = getenv("MONGO_DB", "mongodb+srv://prosaverestrictedcontentbot:Guriya50@cluster0.czrif.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+LOG_GROUP = getenv("LOG_GROUP", "-1004647254401")
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002308280378"))
 FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "0"))
 PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "500"))
 WEBSITE_URL = getenv("WEBSITE_URL", "")
