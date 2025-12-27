3b1d97b70ccbd0d30b55c6d35add29af35c19ee8
Satyajeetkumarofficial <152394869+Satyajeetkumarofficial@users.noreply.github.com>
2025-12-27 16:34:22 +0530

    Update get_func.py

--- devgagan/core/get_func.py
+++ devgagan/core/get_func.py
@@ -606,14 +606,14 @@ async def send_settings_message(chat_id, user_id):
     
     # Define the rest of the buttons
     buttons = [
-        [Button.inline("🆔 Set Chat ID", b'setchat'), Button.inline("Set Rename Tag", b'setrename')],
-        [Button.inline("📝 Caption", b'setcaption'), Button.inline("Replace Words", b'setreplacement')],
-        [Button.inline("🗑️ Remove Words", b'delete'), Button.inline("Reset", b'reset')],
-        [Button.inline("🔐 Session Login", b'addsession'), Button.inline("Logout", b'logout')],
-        [Button.inline("🖼 Set Thumbnail", b'setthumb'), Button.inline("Remove Thumbnail", b'remthumb')],
-        [Button.inline("📄 PDF Wtmrk", b'pdfwt'), Button.inline("Video Wtmrk", b'watermark')],
-        [Button.inline("🎥 Upload Method", b'uploadmethod')],  # Include the dynamic Fast DL button
-        [Button.url("🐞 Report Errors", "https://t.me/ProBotXUpdate")]
+        [Button.inline("🆔 Set Chat ID", b'setchat'), Button.inline("🏷️ Set Rename Tag", b'setrename')],
+        [Button.inline("📝 Caption", b'setcaption'), Button.inline("🔄 Replace Words", b'setreplacement')],
+        [Button.inline("🛑 Remove Words", b'delete'), Button.inline("🧹 Reset", b'reset')],
+        [Button.inline("🔐 Session Login", b'addsession'), Button.inline("🚪 Logout", b'logout')],
+        [Button.inline("🖼 Set Thumbnail", b'setthumb'), Button.inline("🗑️ Remove Thumbnail", b'remthumb')],
+        [Button.inline("📄 PDF Wtmrk", b'pdfwt'), Button.inline("🎥 Video Wtmrk", b'watermark')],
+        [Button.inline("📤 Upload Method", b'uploadmethod')],  # Include the dynamic Fast DL button
+        [Button.url("🆘 Report Errors", "https://t.me/ProBotXUpdate")]
     ]
 
     await gf.send_file(
